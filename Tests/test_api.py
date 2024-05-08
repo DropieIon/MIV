1f3b1f5fcc15401cf711cd84c3046dc2d8f763c6
DropieIon <ciubotaruion@gmail.com>
2024-05-08 14:22:56 +0300

    Irrelevant

--- Tests/test_api.py
+++ Tests/test_api.py
@@ -1,4 +1,4 @@
-from requests import get, post, put
+from requests import get, post, put, delete
 import base64
 
 base_url = 'http://localhost:8000'
@@ -34,8 +34,20 @@ token_tc = post(f'{base_url}/login', json={
 #                    "profile_picB64": ''
 #                })
 
-req_resp = get(f'{base_url}/all_studies', headers={'Authorization': f'Bearer {token_doctor3}'})
+# req_resp = get(f'{base_url}/all_studies', headers={'Authorization': f'Bearer {token_doctor3}'})
 # req_resp = get(f'{base_url}/users/all_doctors/', headers={'Authorization': f'Bearer {token_patient3}'})
+# req_resp = delete(f'{base_url}/acc_data/studies/', headers={'Authorization': f'Bearer {token_doctor1}'},
+#                   json={
+#                       "study_id": "14d74df9-be0fb9f4-a88c5382-55c94913-b46b297a",
+#                       "patient_username": "patient1"
+#                   })
+
+req_resp = put(f'{base_url}/acc_data/studies/unassign', headers={'Authorization': f'Bearer {token_patient1}'},
+                  json={
+                      "study_id": "941a16e6-2b969e8a-d3e9e31a-1bbc74f6-abcad8c3",
+                    #   "study_id": "750255f1-a6d57cdf-6f7692af-b6eb20e8-76b2cd54",
+                      "patient_username": "patient1"
+                  })
 
 print(req_resp.status_code)
 print(req_resp.json())
